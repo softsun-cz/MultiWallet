a657adb5df5332e8ceab7db98a53959cff12418a
AloeSwap <info@aloeswap.com>
2022-02-12 19:01:09 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MultiWallet - smart contracts
+
+1. Edit .secret and put there a wallet mnemonic phrase (24 words) - you need to have some gas on it
+2. Register on polygonscan.com, bscscan.com etc. and create your new API keys
+3. Edit .apikey_* files and add your api keys on the first line of each file (* means block explorer name, e.g.: polygonscan, bscscan ...)
+4. edit ./scripts/deploy.js and set variables
+5. Install dependencies and run deploy script:
+```console
+yarn install
+./deploy.sh
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,182 @@
+const fetch = require('node-fetch');
+
+var netInfo;
+var contracts = [];
+var totalCost = ethers.BigNumber.from('0');
+var verifyScript = '';
+const confirmNum = 1;
+
+async function main() {
+ var addressOne = '0x67Cb8eA92E713b39cd222C0505645B5d1B5907c8';
+ var addressTwo = '0x650E5c6071f31065d7d5Bf6CaD5173819cA72c41';
+ getWelcomeMessage('MultiWallet');
+ netInfo = await getNetworkInfo();
+ getNetworkMessage();
+ console.log();
+ console.log('Deploying smart contracts ...');
+ console.log();
+ var multiWallet = await deploy('MultiWallet');
+ createVerifyScript();
+ getTotalCost();
+ await runFunction(multiWallet, 'addWallet', _addressOne, 7000); // 70%
+ await runFunction(multiWallet, 'addWallet', _addressTwo, 3000); // 30%
+ getTotalCost();
+ await getSummary();
+}
+
+async function getNetworkInfo() {
+ var arr = [];
+ const account = (await ethers.getSigners())[0];
+ arr['chainID'] = (await ethers.provider.getNetwork()).chainId;
+ arr['name'] = 'Unknown';
+ arr['rpc'] = 'Unknown';
+ arr['currency'] = 'Unknown';
+ arr['symbol'] = 'ETH';
+ arr['explorer'] = 'https://etherscan.io';
+ arr['walletAddress'] = account.address;
+ arr['walletBalance'] = ethers.utils.formatEther(await account.getBalance());
+ var response = await fetch('https://chainid.network/chains.json');
+ var json = await response.json();
+ json = JSON.stringify(json);
+ var arrJSON = JSON.parse(json);
+ for (var i = 0; i < arrJSON.length; i++) {
+  if (arrJSON[i].chainId == arr['chainID']) {
+   if (!!arrJSON[i].name) arr['name'] = arrJSON[i].name;
+   if (!!arrJSON[i].nativeCurrency.name) arr['currency'] = arrJSON[i].nativeCurrency.name;
+   if (!!arrJSON[i].nativeCurrency.symbol) arr['symbol'] = arrJSON[i].nativeCurrency.symbol;
+   if (!!arrJSON[i].explorers[0].url) arr['explorer'] = arrJSON[i].explorers[0].url;
+  }
+ }
+ return arr;
+}
+
+function getWelcomeMessage(name) {
+ const eq = '='.repeat(arguments[0].length + 16);
+ console.log();
+ console.log(eq);
+ console.log(name + ' - deploy script');
+ console.log(eq);
+ console.log();
+ console.log('Start time: ' + new Date(Date.now()).toLocaleString());
+ console.log();
+}
+
+function getNetworkMessage() {
+ console.log('Network info:');
+ console.log();
+ console.log('Chain name:      ' + netInfo['name']);
+ console.log('Chain ID:        ' + netInfo['chainID']);
+ console.log('Currency:        ' + netInfo['currency'] + ' (' + netInfo['symbol'] + ')');
+ console.log('Block explorer:  ' + netInfo['explorer']);
+ console.log('Wallet address:  ' + netInfo['walletAddress']);
+ console.log('Wallet balance:  ' + netInfo['walletBalance'] + ' ' + netInfo['symbol']);
+ console.log();
+}
+
+async function deploy() {
+ if (arguments.length == 0) {
+  console.log('Error: Missing smart contract name');
+  console.log();
+  return;
+ }
+ var params = [];
+ if (arguments.length > 1) for (var i = 1; i < arguments.length; i++) params.push(arguments[i]);
+  const dash = '-'.repeat(arguments[0].length + 10);
+ console.log(dash);
+ console.log('Contract: ' + arguments[0]);
+ console.log(dash);
+ console.log();
+ const Contract = await ethers.getContractFactory(arguments[0]);
+ const contract = await Contract.deploy(...params);
+ console.log('Contract TX ID:   ' + contract.deployTransaction.hash);
+ console.log('Contract address: ' + contract.address);
+ var balance = ethers.utils.formatEther(await (await ethers.getSigners())[0].getBalance()) + ' ' + netInfo['symbol'];
+ console.log('Wallet balance:   ' + balance);
+ var result = await contract.deployed();
+ var receipt = await ethers.provider.getTransactionReceipt(contract.deployTransaction.hash);
+ var blockTimestamp = (await ethers.provider.getBlock(receipt.blockNumber)).timestamp;
+ console.log('Block number:     ' + receipt.blockNumber.toString());
+ console.log('Block timestamp:  ' + blockTimestamp.toString());
+ console.log('Gas limit:        ' + result.deployTransaction.gasLimit.toString());
+ console.log('Gas used:         ' + receipt.gasUsed);
+ console.log('Gas price:        ' + ethers.utils.formatUnits(result.deployTransaction.gasPrice.toString(), 'gwei') + ' gwei');
+ console.log('Value sent:       ' + ethers.utils.formatEther(result.deployTransaction.value.toString()) + ' ' + netInfo['symbol']);
+ var cost = contract.deployTransaction.gasPrice.mul(receipt.gasUsed);
+ totalCost = totalCost.add(cost);
+ console.log('Deploy cost:      ' + ethers.utils.formatEther(cost.toString()) + ' ' + netInfo['symbol']);
+ console.log();
+ var cont = [];
+ cont['name'] = arguments[0];
+ cont['address'] = contract.address;
+ contracts.push(cont);
+ console.log('Waiting for ' + confirmNum + ' confirmations...');
+ console.log();
+ var confirmations = 0;
+ var lastConfirmation = -1;
+ while (confirmations < confirmNum) {
+  confirmations = (await contract.deployTransaction.wait(1)).confirmations;
+  if (lastConfirmation != confirmations) console.log('Confirmation: ' + confirmations);
+  lastConfirmation = confirmations;
+ }
+ console.log();
+ verifyScript += 'npx hardhat verify --network $1 --contract contracts/' + arguments[0] + '.sol:' + arguments[0] + ' ' + contract.address;
+ if (arguments.length > 1) for (var i = 1; i < arguments.length; i++) verifyScript += ' "' + arguments[i] + '"';
+ verifyScript += "\n";
+ return result;
+}
+
+function getTotalCost() {
+ var total = 'Total cost: ' + ethers.utils.formatEther(totalCost.toString()) + ' ' + netInfo['symbol'];
+ const eq = '='.repeat(total.length);
+ console.log(eq);
+ console.log(total);
+ console.log(eq);
+ console.log();
+}
+
+function createVerifyScript() {
+ const fs = require('fs');
+ var verifyFile = './verify.sh';
+ if (fs.existsSync(verifyFile)) fs.unlinkSync(verifyFile);
+ fs.writeFileSync(verifyFile, '#!/bin/sh' + "\n\n" + verifyScript);
+ fs.chmodSync(verifyFile, 0o755);
+}
+
+async function getSummary() {
+ console.log('===================');
+ console.log('Deployed contracts:');
+ console.log('===================');
+ console.log();
+ for (var i = 0; i < contracts.length; i++) console.log(contracts[i]['name'] + ': ' + netInfo['explorer'] + '/address/' + contracts[i]['address']);
+ console.log();
+ console.log('End time: ' + new Date(Date.now()).toLocaleString());
+ console.log();
+}
+
+async function runFunction() {
+ if (arguments.length < 2) {
+  console.log('Error: Missing parameters');
+  console.log();
+  return;
+ }
+ var params = [];
+ if (arguments.length > 2) for (var i = 2; i < arguments.length; i++) params.push(arguments[i]);
+ var res = await arguments[0][arguments[1]](...params);
+ if (typeof res === 'object') {
+  console.log('Waiting for 1 confirmation...');
+  await res.wait(1);
+  console.log('Done.');
+  var receipt = await ethers.provider.getTransactionReceipt(res.hash);
+  var cost = res.gasPrice.mul(receipt.gasUsed);
+  console.log('Transaction cost: ' + ethers.utils.formatEther(cost.toString()) + ' ' + netInfo['symbol']);
+  totalCost = totalCost.add(cost);
+  console.log();
+ } else return res;
+}
+
+main()
+ .then(() => process.exit(0))
+ .catch((error) => {
+  console.error(error);
+  process.exit(1);
+ });
